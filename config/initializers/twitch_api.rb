dbdd169c2945004c03701c32ddf3df8d53735f38
Jack M <root@exegete.me>
2020-09-19 05:31:00 -0700

    Added ws to yarn, made twitch api init optional, decreased opacity.

--- config/initializers/twitch_api.rb
+++ config/initializers/twitch_api.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-twitch_token_provider = TwitchTokenProvider.new(
-  client_id: ENV['TWITCH_CLIENT_ID'],
-  client_secret: ENV['TWITCH_CLIENT_SECRET']
-)
+if ENV['TWITCH_CLIENT_ID']
+  twitch_token_provider = TwitchTokenProvider.new(
+    client_id: ENV['TWITCH_CLIENT_ID'],
+    client_secret: ENV['TWITCH_CLIENT_SECRET']
+  )
 
-$TWITCH = Twitch::Client.new(
-  access_token: twitch_token_provider.access_token,
-  client_id: ENV['TWITCH_CLIENT_ID']
-)
+  $TWITCH = Twitch::Client.new(
+    access_token: twitch_token_provider.access_token,
+    client_id: ENV['TWITCH_CLIENT_ID']
+  )
+end
