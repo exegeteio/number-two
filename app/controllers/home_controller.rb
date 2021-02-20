03a0e32f26a1de521d40733b9de31df77e33f5bd
Jack M <root@exegete.me>
2021-02-20 13:11:40 +0000

    Rubocop and home controller test fixes.
    Home controller was not accessible after the get route moved to root.
    Fixed tests for home controller.
    Gave home#index the route name :root.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
+# Home controller for introducing the site.
 class HomeController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Helper methods for the home controller.
 module HomeHelper
 end
