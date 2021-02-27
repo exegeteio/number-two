3387741606a0da9cab2d1ab9a30dd1ef30976208
Jack M <exegete@hey.com>
2021-02-27 01:55:14 +0000

    Added authentication to tests.
    Updated dashboard css and index to keep headers at the top.
    Added authentication to the message channel and dashboard controller tests.
    Added user fixtures.

--- test/channels/message_channel_test.rb
+++ test/channels/message_channel_test.rb
@@ -1,16 +1,26 @@
 require 'test_helper'
 
 class MessageChannelTest < ActionCable::Channel::TestCase
-  test 'subscribes' do
+  setup do
+    @user = users(:default)
+    stub_connection current_user: @user
     subscribe
+  end
+
+  test 'subscribes' do
     assert subscription.confirmed?
   end
 
   test 'creates messages' do
-    subscribe
     channel = MessageChannel.new(connection, {})
     assert_difference 'Message.count' do
       channel.perform_action(Message.first.attributes.except('id'))
     end
+  end
+
+  test 'creates messages for current_user' do
+    channel = MessageChannel.new(connection, {})
+    channel.perform_action(Message.first.attributes.except('id'))
+    assert_equal Message.last.channel, @user.username
   end
 end
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    @user = users(:non_default)
+    sign_in @user
+    get dashboard_url
+    @messages = Message.for_channel(@user.username)
+  end
+
+  test 'should redirect if not authenticated' do
+    sign_out @user
     get dashboard_url(channel: 'default')
+    assert_response :redirect
   end
 
   test 'should get index' do
@@ -12,7 +23,7 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
   test 'should include all messages' do
     assert_response :success
     assert_select '#messages' do
-      assert_select '.message', Message.for_channel('default').count
+      assert_select '.message', @messages.count
     end
   end
 end
